33ccabec8712ffc72b71b609866289207dbd8b83
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-03 20:57:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
 
 const App = () => {
-  //Write your code here
-}
+  const queryParams = new URLSearchParams(window.location.search);
+  const id = queryParams.get('id');
+  const name = queryParams.get('name');
+  const designation = queryParams.get('designation');
+
+  return (
+    <div className="details">
+      Employee {id} named {name} works as {designation}.
+    </div>
+  );
+};
 
 export default App;
+
+
+
+// import React from 'react'
+// import '../styles/App.css';
+
+// const App = () => {
+//   //Write your code here
+// }
+
+// export default App;
